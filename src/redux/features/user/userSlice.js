804e07e47127457b6799d909b5052a3d7e434c3c
Arif uz zaman <arifuzzaman959@gmail.com>
2024-01-05 10:28:32 +0600

    redux thunk and firebase initiated

---
+++ src/redux/features/user/userSlice.js
@@ -0,0 +1,22 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+const initialState = {
+  name: "",
+  email: "",
+  isLoading: true,
+  isError: false,
+  Error: "",
+};
+
+export const createUser = createAsyncThunk("userSlice/createUser", async () => {
+  createUserWithEmailAndPassword();
+});
+
+const userSlice = createSlice({
+  name: "userSlice",
+  initialState,
+  reducers: {},
+});
+
+export default userSlice.reducer;
